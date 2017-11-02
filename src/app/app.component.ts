d4a103e963fa5b6cecdcb67584391b938ea5f83a
Arun Babu N <ab@ameyem.com>
2017-11-02 16:59:58 +0530

    last 2nov17

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'app-root',
+//   // templateUrl: './app.component.html',
+//   template: '<h1>hEY My First {{title}} </h1>'
+
+//   // styleUrls: ['./app.component.css']
+// })
+// export class AppComponent {
+//   title = 'Angular Project!!';
+// }
 import { Component } from '@angular/core';
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+@Component ({
+   selector: 'app-root',
+   template: ` <div *ngIf = 'appStatus'>{{appTitle}} Tutorialspoint </div> 
+   <div *ngFor = 'let variable of variablelist'>
+    <ul> 
+      <li>{{variable.ID}}</li> 
+      <li>{{variable.Name}}</li> 
+   </ul> 
+</div>
+    `,
 })
+
 export class AppComponent {
-  title = 'app';
+   appTitle: string = 'Welcome to';
+appStatus: boolean = true;
+ variablelist: any[] = [ {
+      "ID": "1",
+      "Name" : "One"
+   },
+
+   {
+      "ID": "2",
+      "Name" : "Two"
+   } ];
 }
+
